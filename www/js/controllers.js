4b9d6057b61811c45ea577ded9134ab4e30904d5
an-circle <562171119@qq.com>
2017-12-01 14:20:55 +0800

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,66 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+    $scope.title='大米';
+    $scope.products=[{
+      id:1,
+      name:'铁三角耳机',
+      desc:'123',
+      price:'3899'
+
+
+    },{
+      id:2,
+      name:'铁三角耳机',
+      desc:'123',
+      price:'3899'
+
+
+    },{
+      id:3,
+      name:'铁三角耳机',
+      desc:'123',
+      price:'3899'
+
+
+    }];
+
+
+
+
+
+})
+
+  .controller('detailCtrl', function($scope,$stateParams) {
+
+$scope.id=$stateParams.id;
+
+
+
+
+  })
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
